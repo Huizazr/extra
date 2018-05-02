529101ca1b71a2340ccd452a620960e5ef1a0534
Carlos0105 <38895739+Carlos0105@users.noreply.github.com>
2018-05-01 19:54:37 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$dbname = "registros";
+
+$conexion = mysqli_connect($server, $user, $password, $dbname);
+
+if (!$conexion) {
+	die("Error: " . mysqli_connect_error());
+}
+?>
